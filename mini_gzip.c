db6d92475543eea786b4d8e3edc3fea2499037ca
舒俊杰 <admin@shuax.com>
2017-06-27 19:31:57 +0800

    add gzip compress

--- mini_gzip.c
+++ mini_gzip.c
@@ -1,9 +1,9 @@
 /*
  * BSD 2-clause license
  * Copyright (c) 2013 Wojciech A. Koszek <wkoszek@FreeBSD.org>
- * 
+ *
  * Based on:
- * 
+ *
  * https://github.com/strake/gzip.git
  *
  * I had to rewrite it, since strake's version was powered by UNIX FILE* API,
@@ -21,8 +21,105 @@
 #include "miniz.h"
 #include "mini_gzip.h"
 
+static void *gzip_compress(uint8_t *data, size_t len, size_t *out_len)
+{
+    int flush;
+    int status;
+    int buf_len;
+    void *buf;
+    z_stream strm;
+
+    /* Allocate buffer */
+    buf_len = len + 32;
+    buf = malloc(buf_len);
+    if (!buf) {
+        return NULL;
+    }
+
+    /*
+     * Miniz don't support GZip format directly, instead we will:
+     *
+     * - append manual GZip magic bytes
+     * - inflate raw content
+     * - append manual CRC32 data
+     */
+
+    /* GZIP Magic bytes */
+    uint8_t *pb = (uint8_t*)buf;
+
+    pb[0] = 0x1F;
+    pb[1] = 0x8B;
+    pb[2] = 8;
+    pb[3] = 0;
+    pb[4] = 0;
+    pb[5] = 0;
+    pb[6] = 0;
+    pb[7] = 0;
+    pb[8] = 0;
+    pb[9] = 0xFF;
+    pb += 10;
+
+    /* Prepare streaming buffer context */
+    memset(&strm, '\0', sizeof(strm));
+    strm.zalloc = Z_NULL;
+    strm.zfree  = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.next_in   = data;
+    strm.avail_in  = len;
+    strm.total_out = 0;
+
+    flush = Z_NO_FLUSH;
+    deflateInit2(&strm, Z_BEST_COMPRESSION,
+                 Z_DEFLATED, -Z_DEFAULT_WINDOW_BITS, 9, Z_DEFAULT_STRATEGY);
+
+    while (1) {
+        strm.next_out  = pb + strm.total_out;
+        strm.avail_out = buf_len - strm.total_out;
+
+        if (strm.avail_in == 0) {
+            flush = Z_FINISH;
+        }
+
+        status = deflate(&strm, flush);
+        if (status == Z_STREAM_END) {
+            break;
+        }
+        else if (status != Z_OK) {
+            deflateEnd(&strm);
+            free(buf);
+            return NULL;
+        }
+    }
+
+    if (deflateEnd(&strm) != Z_OK) {
+        free(buf);
+        return NULL;
+    }
+    *out_len = strm.total_out;
+
+    /* Construct the GZip CRC32 (footer) */
+    mz_ulong crc;
+    int footer_start = strm.total_out + 10;
+
+    crc = mz_crc32(MZ_CRC32_INIT, data, len);
+    pb = (uint8_t*)buf;
+    pb[footer_start] = crc & 0xFF;
+    pb[footer_start + 1] = (crc >> 8) & 0xFF;
+    pb[footer_start + 2] = (crc >> 16) & 0xFF;
+    pb[footer_start + 3] = (crc >> 24) & 0xFF;
+    pb[footer_start + 4] = len & 0xFF;
+    pb[footer_start + 5] = (len >> 8) & 0xFF;
+    pb[footer_start + 6] = (len >> 16) & 0xFF;
+    pb[footer_start + 7] = (len >> 24) & 0xFF;
+
+    /* Set the real buffer size for the caller */
+    *out_len += 10 + 8;
+
+    return buf;
+}
+
 int
-mini_gz_start(struct mini_gzip *gz_ptr, void *mem, size_t mem_len)
+mini_gz_start(struct mini_gzip *gz_ptr, const void *mem, size_t mem_len)
 {
 	uint8_t		*hptr, *hauxptr, *mem8_ptr;
 	uint16_t	fextra_len;
@@ -40,7 +137,7 @@ mini_gz_start(struct mini_gzip *gz_ptr, void *mem, size_t mem_len)
 	gz_ptr->chunk_size = 1024;
 
 	if (hptr[0] != 0x1F || hptr[1] != 0x8B) {
-		GZDBG("hptr[0] = %02x hptr[1] = %02x\n", hptr[0], hptr[1]);
+		//GZDBG("hptr[0] = %02x hptr[1] = %02x\n", hptr[0], hptr[1]);
 		return (-1);
 	}
 	if (hptr[2] != 8) {
@@ -110,7 +207,7 @@ mini_gz_unpack(struct mini_gzip *gz_ptr, void *mem_out, size_t mem_out_len)
 	in_bytes_avail = gz_ptr->data_len;
 	s.avail_out = mem_out_len;
 	s.next_in = gz_ptr->data_ptr;
-	s.next_out = mem_out;
+	s.next_out = (unsigned char*)mem_out;
 	for (;;) {
 		bytes_to_read = MINI_GZ_MIN(gz_ptr->chunk_size, in_bytes_avail);
 		s.avail_in += bytes_to_read;
